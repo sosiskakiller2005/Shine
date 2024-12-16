cd65597ed509ee9994ff8df08e78fd4a2ef41717
Влад Козлов <vkozlov15012005@gmail.com>
2024-12-16 21:56:28 +0300

    Добавьте файлы проекта.

---
+++ Shine/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Shine.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Shine
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static ShineDbEntities _context;
+        public static ShineDbEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new ShineDbEntities();
+            }
+            return _context;
+        }
+    }
+}
